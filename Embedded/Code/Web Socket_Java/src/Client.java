5828551d609ec01b2264efe421fc2e95ec3e7057
RozaHasso <44085005+RozaHasso@users.noreply.github.com>
2019-04-24 20:39:28 +0200

    socket

--- Embedded/Code/Web Socket_Java/src/Client.java
+++ Embedded/Code/Web Socket_Java/src/Client.java
@@ -1,35 +1,32 @@
-
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
-
-import sun.awt.CharsetString;
-
-import java.util.Arrays;
-import java.util.Base64;
-
 import com.google.gson.Gson;
-import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+import com.google.gson.JsonElement;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
 
-import javax.crypto.Cipher;
-import org.w3c.dom.*;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.*;
-import java.io.*;
-import java.math.BigInteger;
 
 //Client class is responsible for receiving measurements from Loriet server using a socket connection
 
 public class Client
 {
 
+   private static final JsonElement takeCo2Measurement = null;
+   private int port = 80;
    private Socket socket;
+   private String ip = "";
+   
+   
 
    private DataOutputStream out;
    private DataInputStream in;
-  
+   
+   MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://fadi:Fidodido1940@fadi-pwbgj.mongodb.net/test?retryWrites=true"));
+
    Gson gson;
 
    public void connect(String ip, int port)
@@ -45,26 +42,19 @@ public class Client
       {
          e.printStackTrace();
       }
-      gson = new Gson();
-
-      // -----------PROTOCOL START--------------
-
-      
-      // ------------ PROTOCOL END ---------------
-
-   }
-
-   //not specified which measurements will be received yet
-   public void get(Measurement measurement) throws Exception
-   {
-
-      out.read(gson.toJson(measurement).getBytes(StandardCharsets.UTF_8));
-
    }
 
    
-    //Closing the connection.
-    
+
+   public void get(Co2Measurement co2) throws Exception
+   {
+
+      in.read(gson.toJson(co2).getBytes(StandardCharsets.UTF_8));
+
+   }
+
+   // Closing the connection.
+
    public void disconnet()
    {
       try
